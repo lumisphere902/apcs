a7542dddb7d581e588c5424589b544d972615221
lumisphere902 <tpjunk0@gmail.com>
2016-12-12 16:28:27 -0800

    Update P1_Poon_Timothy_MyFractal.java

--- P1_Poon_Timothy_MyFractal.java
+++ P1_Poon_Timothy_MyFractal.java
@@ -1,17 +1,6 @@
-//Timothy Poon, Period 1 10/1/2016
+//Timothy Poon 10/1/2016
 //Heighway Dragon
-//~50 minutes
-//******************************************************************************
-//Overall, I think that this lab was the most interesting, as well as
-//challenging, lab of the year. Although not much technical expertise or
-//complicated problem solving is required for this program, I still went down
-//multiple incorrect paths before realizing what I should be doing. When I saw
-//the fractals of level 1 and 2, I thought I knew what I should be doing, and
-//implemented that. However, after some quick testing, I realized that the
-//pattern I thought I saw didn't continue into higher levels of the fractal.
-//After creating the base fractal, I decided to tackle the problem of creating
-//the actual dragon-like image. Although it seemed daunting at first, I soon
-//realized that I only needed to modify a few numbers to create the image.
+
 import java.awt.Color;
 import gpdraw.*;
 
